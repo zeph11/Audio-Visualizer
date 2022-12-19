3c9753c783b8fbfda1512d42ce97448d24271c9c
zeph111 <luniva121@gmail.com>
2022-12-19 20:44:57 +0545

    hi

--- src/miniproject.py
+++ src/miniproject.py
@@ -3,24 +3,38 @@ import numpy as np
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 from audio_analyzer import fft_points
-from helpers import altList,toNVC2
+from helpers import altList, toNVC2
 
 
 RESOLUTION = 800
-
+x_change = float(0.0)
+numberofspikes = 0
 
 
 def window_resize(window, width, height):
     glViewport(0, 0, width, height)
 
-def main():
-    
-    #after how much to trigger it
-    time= float(5.0)
 
-    previous=float(glfw.get_time())
-    
-    vertex_src="""
+def subbufferfun():
+    current_value_x = -1 + numberofspikes*x_change
+    vertices = [
+        current_value_x, 0.5, 0.0,
+        current_value_x+x_change, 0.5, 0.0,
+        current_value_x, 0.0, 0.0,
+        current_value_x+x_change, 0.0, 0.0,
+    ]
+
+    glBufferSubData(GL_ARRAY_BUFFER, numberofspikes*24, 24, vertices)
+
+
+def main():
+
+    # after how much to trigger it
+    time = float(10.0)
+
+    previous = float(glfw.get_time())
+
+    vertex_src = """
     
     #version 330
     layout(location=0) in vec2 aPos;
@@ -32,8 +46,8 @@ def main():
         
         }
     """
-    
-    fragment_src ="""
+
+    fragment_src = """
     
     #version 330
     
@@ -45,7 +59,7 @@ def main():
         }
     
     """
-    
+
     if not glfw.init():
         raise Exception("glfw can not be initialized!")
 
@@ -55,18 +69,16 @@ def main():
         glfw.terminate()
         raise Exception("glfw window can not be created!")
 
-
     # glfw.set_window_pos(window,100,100)
     glfw.set_window_size_callback(window, window_resize)
     glfw.make_context_current(window)
-        
-    y_coordinates= [abs(i) for i in fft_points()]
 
-    if len(y_coordinates)>RESOLUTION -100:
-        y_coordinates=y_coordinates[0:RESOLUTION-100]
-        
+    y_coordinates = [abs(i) for i in fft_points()]
 
-    x_coordinates= [i for i in range(-RESOLUTION,RESOLUTION)]
+    if len(y_coordinates) > RESOLUTION - 100:
+        y_coordinates = y_coordinates[0:RESOLUTION-100]
+
+    x_coordinates = [i for i in range(-RESOLUTION, RESOLUTION)]
     translation = np.array(
         [
             1.0,
@@ -89,45 +101,43 @@ def main():
         dtype=np.float32,
     )
 
-    vertices = toNVC2(altList(x_coordinates,y_coordinates),RESOLUTION)
-    
+    vertices = toNVC2(altList(x_coordinates, y_coordinates), RESOLUTION)
 
-    vertices = np.array(vertices,dtype=np.float32)
-    
-    shader =  compileProgram(compileShader(vertex_src,GL_VERTEX_SHADER),compileShader(fragment_src,GL_FRAGMENT_SHADER))
+    vertices = np.array(vertices, dtype=np.float32)
+
+    shader = compileProgram(compileShader(
+        vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
     print(vertices)
     vertex_buffer_object = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER,vertex_buffer_object)
-    glBufferData(GL_ARRAY_BUFFER,vertices.nbytes,vertices,GL_STREAM_DRAW)
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object)
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STREAM_DRAW)
     # element_buffer_object= glGenBuffers(1)
     # glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,element_buffer_object)
     # glBufferData(GL_ELEMENT_ARRAY_BUFFER,indices.nbytes,indices,GL_STATIC_DRAW)
 
-    
     glEnableVertexAttribArray(0)
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, vertices.itemsize*2, ctypes.c_void_p(0))
-    
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,
+                          vertices.itemsize*2, ctypes.c_void_p(0))
+
     glUseProgram(shader)
 
     glClearColor(0.1, 0.1, 0.1, 0.1)
     transformation_location = glGetUniformLocation(shader, "translation")
-    
 
     while not glfw.window_should_close(window):
         glfw.poll_events()
-        now=float(glfw.get_time())
-        delta=float(now-previous)
-        previous=now
-        
-        #for each timer
-        time-=delta
-        if (time<=0.0):
-            #timer triggerthing need to do here
-            glfw.terminate()
-       
-        glClear(GL_COLOR_BUFFER_BIT)
+        now = float(glfw.get_time())
+        delta = float(now-previous)
+        previous = now
+
+        # for each timer
+        time -= delta
+        if (time <= 0.0):
+            # timer triggerthing need to do here
+            subbufferfun()
+
         glUniformMatrix4fv(transformation_location, 1, GL_FALSE, translation)
-        glDrawArrays(GL_LINES,0,len(y_coordinates)*2)
+        glDrawArrays(GL_LINES, 0, len(y_coordinates)*2)
         glfw.swap_buffers(window)
     glfw.terminate()
 
